a4e861f597993e9a23de67688f0f76fb0c035172
chenbitao <cbtpro@163.com>
2023-06-12 21:19:19 +0800

    feat: 增加mock数据

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { MockController } from './controller/mock.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, MockController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/controller/mock.controller.ts
@@ -0,0 +1,42 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { tableList } from './mock-list';
+
+@Controller('/mock')
+export class MockController {
+  @Get('/list')
+  getList(@Query() params: PagingParams): IResponseBodyByPaging<IMockUser> {
+    const len = tableList.length;
+    const [pageSize, page] = [
+      Number.parseInt(params.pageSize),
+      Number.parseInt(params.page),
+    ];
+
+    const totalPages = Math.ceil(len / pageSize);
+
+    const currentPage = page < 0 ? 0 : page > totalPages ? totalPages : page;
+
+    const currentPageData = tableList.slice(
+      currentPage * pageSize,
+      (currentPage + 1) * pageSize,
+    );
+
+    const responseBody: IResponseBodyByPaging<IMockUser> = {
+      success: true,
+      message: '获取数据成功！',
+      data: currentPageData,
+      meta: {
+        /** 本页记录数 */
+        itemCount: currentPageData.length,
+        /** 总记录数 */
+        totalItems: len,
+        /** 每页请求几页 */
+        itemsPerPage: pageSize,
+        /** 总页数 */
+        totalPages: totalPages,
+        /** 当前第几页 */
+        currentPage: currentPage,
+      },
+    };
+    return responseBody;
+  }
+}
